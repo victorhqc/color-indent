244a0c4656e15ce352f3ac5f3b611fea9ba2bba4
Victor Quiroz <victorhqc@gmail.com>
2017-10-19 15:23:26 +0200

    init default package

---
+++ spec/color-indent-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ColorIndentView from '../lib/color-indent-view';
+
+describe('ColorIndentView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
